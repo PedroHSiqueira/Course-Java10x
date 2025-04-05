f2bad613f742114247c993f68a2c8fd42853f028
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-04-05 16:40:17 -0300

    ADD: Trabalhando melhor com Enums

--- intermediario/Models/Senioridade.java
+++ intermediario/Models/Senioridade.java
@@ -1,7 +1,31 @@
 package intermediario.Models;
 
 public enum Senioridade {
-    JUNIOR,
-    PLENO,
-    SENIOR
+    JUNIOR("Nivel mais baixo na hierarquia", 2),
+    PLENO("Nivel médio na hierarquia", 6),
+    SENIOR("Nivel alto na hierarquia", 11);
+
+    private String descricao;
+    private int anosExperiencia;
+
+    Senioridade(String descricao, int anosExperiencia) {
+        this.descricao = descricao;
+        this.anosExperiencia = anosExperiencia;
+    }
+
+    public int getAnosExperiencia() {
+        return anosExperiencia;
+    }
+
+    public void setAnosExperiencia(int anosExperiencia) {
+        this.anosExperiencia = anosExperiencia;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
