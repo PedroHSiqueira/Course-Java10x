be49136fe929a1139dca1c4eb48be66aef86af23
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-05-04 14:06:55 -0300

    Feat: Inicio ao BigO Notation

---
+++ avançado/BigO/BigOPerformance.java
@@ -0,0 +1,43 @@
+package avançado.BigO;
+
+import intermediario.enums.Senioridade;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BigOPerformance {
+    public static void main(String[] args) {
+        /*
+        * Para realizar ações como Adição, exclusão, etc...
+        * o LinkedList tem uma perfmormance Superior ao Arraylist, dependendo do tamanho do mesmo
+        * Sendo De complexidade O(n) - Constante
+        * */
+
+        long inicio;
+        long fim;
+        long total;
+
+        List<Integer> numerosArray = new ArrayList<>();
+        LinkedList<Integer> numerosLinkedList = new LinkedList<>();
+
+        for (int i = 0; i < 50000000; i++) {
+            numerosArray.add(i);
+            numerosLinkedList.add(i);
+        }
+
+        inicio = System.nanoTime();
+        numerosArray.add(500);
+        fim = System.nanoTime();
+        total = (fim - inicio);
+        System.out.println("Tempo arrayList: " + total + " ns");
+
+        inicio = System.nanoTime();
+        numerosLinkedList.add(500);
+        fim = System.nanoTime();
+        total = (fim - inicio);
+        System.out.println("Tempo linkedList: " + total + " ns");
+
+
+    }
+}
